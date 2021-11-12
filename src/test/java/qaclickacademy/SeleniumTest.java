1a6ecac5b5b6d0691f08168b1e9d1e2aea71d9a2
pratik <pratik1718@yahoo.com>
2021-11-12 11:16:44 -0500

    first commit

---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,21 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	//TESTNG XML FILE FROM MAVEN how to execute all test case from test folder - mvn test 
+	
+	@Test
+	public void BrowserAutomation()
+	{
+		System.out.println("BrowserAutomation");
+	}
+	
+	@Test
+	public void elementsUI()
+	{
+		System.out.println("elementsUI");
+	}
+
+}

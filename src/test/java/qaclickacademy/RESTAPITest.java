d8cf69b117dd460761c1b26f6190acc95d6c60de
pratik <pratik1718@yahoo.com>
2021-11-12 13:11:56 -0500

    American guy commit

--- src/test/java/qaclickacademy/RESTAPITest.java
+++ src/test/java/qaclickacademy/RESTAPITest.java
@@ -11,6 +11,12 @@ public class RESTAPITest {
 		System.out.println("PostJira");
 		System.out.println("PostJira1");
 		System.out.println("PostJira2");
+		System.out.println("PostJira3");
+		System.out.println("PostJira4");
+		System.out.println("PostJira2");
+		System.out.println("PostJira");
+		System.out.println("PostJira1");
+		System.out.println("PostJira2");
 	}
 	
 	@Test
